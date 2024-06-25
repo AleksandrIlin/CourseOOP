9094de9b280e2b1f0b20e793e4f4714b20c4eb60
Aleksandr <aqwa06@gmail.com>
2024-06-25 05:16:54 +0500

    creating test_utils.py, README.md fix linters

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,18 @@
 import json
 import os
+from typing import Any
 
 
 class Category:
-    """ Класс категория"""
+    """Класс категория"""
+
     name: str
     description: str
     products: list
     all_category = 0
     all_unique_products = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name: Any, description: Any, products: Any) -> None:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам."""
         self.name = name
         self.description = description
@@ -21,32 +23,55 @@ class Category:
 
 
 class Product:
-    """ Класс продукт"""
+    """Класс продукт"""
+
     name: str
     description: str
     price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: Any, description: Any, price: Any, quantity: Any) -> None:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам."""
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
 
-    def __repr__(self):
-        return (f'Product(name={self.name}, description={self.description},'
-                f'price={self.price}, quantity={self.quantity})')
+    def __repr__(self) -> Any:
+        return (
+            f"Product(name={self.name}, description={self.description}, price={self.price}, quantity={self.quantity})"
+        )
 
 
-def load_data_from_file():
-    """Загрузка данных из файла .json"""
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_dir, "../data", "products.json")
-    with open(file_path, "r", encoding='utf-8') as file:
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "../data", "products.json")
+
+
+def load_data_from_json(file_path: str) -> Any:
+    with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
-    for product in data:
-        products_list = []
-        for prod in product['products']:
-            products_list.append(Product(prod['name'], prod['description'], prod['price'], product['quantity']))
-        Category(product['name'], product['description'], products_list)
+        categories = []
+        for category_data in data:
+            name = category_data["name"]
+            description = category_data["description"]
+            products_data = category_data["products"]
+            products = []
+            for product_data in products_data:
+                product = Product(
+                    product_data["name"], product_data["description"], product_data["price"], product_data["quantity"]
+                )
+                products.append(product)
+            category = Category(name, description, products)
+            categories.append(category)
+        return categories
+
+
+categories = load_data_from_json(file_path)
+for category in categories:
+    print(category.name)
+    print(category.description)
+    for product in category.products:
+        print(product.name)
+        print(product.description)
+        print(product.price)
+        print(product.quantity)
