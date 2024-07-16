2b7a708d4b04e68c3c6be49291da9d712dfda4ca
Aleksandr <aqwa06@gmail.com>
2024-07-16 16:21:59 +0500

    creating class BaseProduct, CreationMixin

--- src/subclass.py
+++ src/subclass.py
@@ -1,18 +1,27 @@
-from src.utils import Product
+from src.utils import Product, CreationMixin
 
 
-class Smartphone(Product):
+class Smartphone(Product, CreationMixin):
+    """Класс смартфон"""
     def __init__(self, name, description, price, quantity, color, performance, model, memory):
-        super().__init__(name, description, price, quantity, color)
         self.performance = performance
         self.model = model
         self.memory = memory
-
-
-
-class GrassLawn(Product):
-    def __init__(self, name, description, price, quantity, color, country, germination_time):
         super().__init__(name, description, price, quantity, color)
+
+    def new_product(self, *args, **kwargs):
+        """Реализация метода new_product"""
+        pass
+
+
+class GrassLawn(Product, CreationMixin):
+    """Класс трава газонная"""
+    def __init__(self, name, description, price, quantity, color, country, germination_time):
         self.country = country
         self.germination_time = germination_time
+        super().__init__(name, description, price, quantity, color)
+
+    def new_product(self, *args, **kwargs):
+        """Реализация метода new_product"""
+        pass
 
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,27 @@
 import json
 from typing import Any
+from abc import ABC, abstractmethod
+
+
+class BaseProduct(ABC):
+    """Базовый класс"""
+    @abstractmethod
+    def new_product(self, *args, **kwargs):
+        """Абстрактный метод"""
+        pass
+
+
+class CreationMixin:
+    """Класс миксин"""
+
+    def __init__(self, *args, **kwargs):
+        """Инициализация класса миксин"""
+        super().__init__(*args, **kwargs)
+        print(repr(self))
+
+    def __repr__(self):
+        """Вывод для разработчика"""
+        return f"Создан объект класса {self.__class__.__name__}: {self}"
 
 
 class Category:
@@ -21,44 +43,55 @@ class Category:
         Category.all_category += 1
 
     def __len__(self) -> int:
+        """Получение суммы продуктов в категории"""
         return sum(product.quantity for product in self.__products)
 
     def __str__(self) -> str:
+        """Вывод количество продуктов в категории"""
         return f"{self.name}, количество продуктов: {len(self)} шт"
 
     @classmethod
     def set_products(cls, list_products: list) -> None:
+        """Получение списка продуктов"""
         cls.__products = list_products
 
     def get_products(self) -> list:
+        """Получение приватного продукта"""
         return self.__products
 
 
-class Product:
+class Product(BaseProduct, CreationMixin):
     """Класс продукт"""
-
     name: str
     description: str
     _price: float
     quantity: int
     color: str
 
-    def __init__(self, name: str, description: str, price: float, quantity: int, color: str) -> None:
+    def __init__(self, name: str, description: str, price: float, quantity: int, color: str, *args, **kwargs) -> None:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам."""
         self.name = name
         self.description = description
         self._price = price
         self.quantity = quantity
         self.color = color
+        super().__init__(*args, **kwargs)
 
     def __str__(self) -> str:
         """Метод для вывода строки"""
         return f"{self.name}, {self._price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        if type(self) != type(other):
-            raise TypeError("Можно складывать только одинаковые типы продуктов")
-        return self.price * self.quantity + other.price * other.quantity
+        """Метод сложения товаров одного класса"""
+        if isinstance(other, self.__class__):
+            result = self._price * self.quantity + other.price * other.quantity
+            return result
+        else:
+            raise TypeError("Можно складывать только товары одного класса")
+
+    def new_product(self, *args, **kwargs):
+        """Реализация метода new_product"""
+        pass
 
     @classmethod
     def create_and_add_to_list(
